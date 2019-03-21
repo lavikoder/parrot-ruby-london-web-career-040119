2c643553555916b76d14ecf0b23ea5915fdf83fe
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-21 15:08:05 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,5 @@
 def parrot(arg = "Squawk!")
-  puts " This is the sound od a parrot, #{arg}, in the morning."
+  puts " This is the sound of a parrot, #{arg}, in the morning."
 end 
 
 parrot("Hey")
